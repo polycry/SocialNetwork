6a260f2f94b8ef657c51a9f35eb2bd291198b333
polycry <breitenberger.jonas@gmail.com>
2018-04-04 15:46:13 +0200

    Von Feg für feg

--- app/src/main/java/com/feg/socialnetwork/AddPostActivity.java
+++ app/src/main/java/com/feg/socialnetwork/AddPostActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import org.json.JSONException;
@@ -14,7 +15,7 @@ import java.util.HashMap;
 
 public class AddPostActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button post;
+    ImageButton post;
     EditText post_content;
     String logged_in_user;
 
